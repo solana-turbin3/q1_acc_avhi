f1de9c21a668764c98333833fc3c4ef3227cdbbd
AvhiMaz <avhimazumder5@outlook.com>
2026-02-07 16:56:02 +0530

    chore: update error
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- transfer-hook-vault/programs/transfer-hook-vault/src/error.rs
+++ transfer-hook-vault/programs/transfer-hook-vault/src/error.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Invalid account size")]
+    InvalidAccountSize,
+    #[msg("Mint initialization failed")]
+    MintInitializationFailed,
+    #[msg("Extension initialization failed")]
+    ExtensionInitializationFailed,
+    #[msg("Unauthorized")]
+    Unauthorized,
 }
