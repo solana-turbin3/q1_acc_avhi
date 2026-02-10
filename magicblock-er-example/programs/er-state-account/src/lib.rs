58694b8159db091fa953f455ef3b034936a152fe
AvhiMaz <avhimazumder5@outlook.com>
2026-02-10 10:01:10 +0530

    chore: add scaffold
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ magicblock-er-example/programs/er-state-account/src/lib.rs
@@ -0,0 +1,56 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)]
+
+use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::anchor::ephemeral;
+
+mod state;
+mod instructions;
+
+use instructions::*;
+
+declare_id!("9hG187VazKdEZcYbsEcoPuPEWwkfF9HccUDTAJzuEcg3");
+
+#[ephemeral]
+#[program]
+pub mod er_state_account {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<InitUser>) -> Result<()> {
+        ctx.accounts.initialize(&ctx.bumps)?;
+        
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<UpdateUser>, new_data: u64) -> Result<()> {
+        ctx.accounts.update(new_data)?;
+        
+        Ok(())
+    }
+
+    pub fn update_commit(ctx: Context<UpdateCommit>, new_data: u64) -> Result<()> {
+        ctx.accounts.update_commit(new_data)?;
+        
+        Ok(())
+    }
+
+    pub fn delegate(ctx: Context<Delegate>) -> Result<()> {
+        ctx.accounts.delegate()?;
+        
+        Ok(())
+    }
+
+    pub fn undelegate(ctx: Context<Undelegate>) -> Result<()> {
+        ctx.accounts.undelegate()?;
+        
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<CloseUser>) -> Result<()> {
+        ctx.accounts.close()?;
+        
+        Ok(())
+    }
+}
+
