206198e3d3f7ec142d987dd34e71e979e540f2a8
AvhiMaz <avhimazumder5@outlook.com>
2026-02-17 18:42:29 +0530

    feat: test with data
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- generic-storage/src/main.rs
+++ generic-storage/src/main.rs
@@ -19,6 +19,22 @@ struct Storage<T, S> {
     _marker: PhantomData<T>,
 }
 
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    SchemaRead,
+    SchemaWrite,
+    BorshSerialize,
+    BorshDeserialize,
+    serde::Serialize,
+    serde::Deserialize,
+)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
 impl<T: BorshSerialize + BorshDeserialize> Serializer<T> for Borsh {
     fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         borsh::to_vec(value).map_err(|e| e.into())
@@ -77,5 +93,28 @@ impl<T, S: Serializer<T>> Storage<T, S> {
 }
 
 fn main() {
-    todo!();
+    let person = Person {
+        name: "avhi".to_string(),
+        age: 21,
+    };
+
+    let mut borsh_storage = Storage::new(Borsh);
+    let data = borsh_storage.has_data();
+    assert_eq!(data, false);
+    borsh_storage.save(&person).unwrap();
+
+    let load = borsh_storage.load().unwrap();
+    assert_eq!(person, load);
+
+    let mut serde_storage = Storage::new(SerdeJson);
+    let data = serde_storage.has_data();
+    assert_eq!(data, false);
+    serde_storage.save(&person).unwrap();
+    assert_eq!(serde_storage.load().unwrap(), person);
+
+    let mut wincode_storage = Storage::new(Wincode);
+    let data = wincode_storage.has_data();
+    assert_eq!(data, false);
+    wincode_storage.save(&person).unwrap();
+    assert_eq!(wincode_storage.load().unwrap(), person);
 }
