ebaf5d8e38dedd2a7cc9b0c8455213a4888d0dcb
AvhiMaz <avhimazumder5@outlook.com>
2026-02-17 11:21:26 +0530

    feat: implement three serializers
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- generic-storage/src/main.rs
+++ generic-storage/src/main.rs
@@ -7,6 +7,40 @@ trait Serializer<T> {
     fn from_bytes(&self, bytes: &[u8]) -> Result<T, Box<dyn std::error::Error>>;
 }
 
+struct Borsh;
+struct Wincode;
+struct SerdeJson;
+
+impl<T: BorshSerialize + BorshDeserialize> Serializer<T> for Borsh {
+    fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        borsh::to_vec(value).map_err(|e| e.into())
+    }
+
+    fn from_bytes(&self, bytes: &[u8]) -> Result<T, Box<dyn std::error::Error>> {
+        borsh::from_slice(bytes).map_err(|e| e.into())
+    }
+}
+
+impl<T: SchemaWrite<DefaultConfig, Src = T> + for<'a> SchemaRead<'a, DefaultConfig, Dst = T>>
+    Serializer<T> for Wincode
+{
+    fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        wincode::serialize(value).map_err(|e| e.into())
+    }
+    fn from_bytes(&self, bytes: &[u8]) -> Result<T, Box<dyn std::error::Error>> {
+        wincode::deserialize(bytes).map_err(|e| e.into())
+    }
+}
+
+impl<T: Serialize + DeserializeOwned> Serializer<T> for SerdeJson {
+    fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        serde_json::to_vec(value).map_err(|e| e.into())
+    }
+    fn from_bytes(&self, bytes: &[u8]) -> Result<T, Box<dyn std::error::Error>> {
+        serde_json::from_slice(bytes).map_err(|e| e.into())
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
