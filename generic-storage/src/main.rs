5a97b9d516492f54d9ae32615e8e15054adfa850
AvhiMaz <avhimazumder5@outlook.com>
2026-02-17 11:20:24 +0530

    feat: define a serializer trait
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- generic-storage/src/main.rs
+++ generic-storage/src/main.rs
@@ -1,3 +1,12 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use serde::{Serialize, de::DeserializeOwned};
+use wincode::{SchemaRead, SchemaWrite, config::DefaultConfig};
+
+trait Serializer<T> {
+    fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
+    fn from_bytes(&self, bytes: &[u8]) -> Result<T, Box<dyn std::error::Error>>;
+}
+
 fn main() {
     println!("Hello, world!");
 }
