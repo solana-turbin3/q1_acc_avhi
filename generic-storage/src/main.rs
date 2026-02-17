03ac2ecbe539847746fb4985122e4f2044f1a6ad
AvhiMaz <avhimazumder5@outlook.com>
2026-02-17 18:57:23 +0530

    feat: add tests
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- generic-storage/src/main.rs
+++ generic-storage/src/main.rs
@@ -118,3 +118,47 @@ fn main() {
     wincode_storage.save(&person).unwrap();
     assert_eq!(wincode_storage.load().unwrap(), person);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_borsh() {
+        let person = Person {
+            name: "avhi".to_string(),
+            age: 21,
+        };
+
+        let mut storage = Storage::new(Borsh);
+        assert_eq!(storage.has_data(), false);
+        storage.save(&person).unwrap();
+        assert_eq!(storage.load().unwrap(), person);
+    }
+
+    #[test]
+    fn test_wincode() {
+        let person = Person {
+            name: "avhi".to_string(),
+            age: 21,
+        };
+
+        let mut storage = Storage::new(Borsh);
+        assert_eq!(storage.has_data(), false);
+        storage.save(&person).unwrap();
+        assert_eq!(storage.load().unwrap(), person);
+    }
+
+    #[test]
+    fn test_serde_json() {
+        let person = Person {
+            name: "avhi".to_string(),
+            age: 21,
+        };
+
+        let mut storage = Storage::new(Borsh);
+        assert_eq!(storage.has_data(), false);
+        storage.save(&person).unwrap();
+        assert_eq!(storage.load().unwrap(), person);
+    }
+}
