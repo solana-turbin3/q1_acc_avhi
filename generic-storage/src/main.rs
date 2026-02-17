1656326bef12a2d29fa653dad445268a3e492b81
AvhiMaz <avhimazumder5@outlook.com>
2026-02-17 17:40:16 +0530

    feat: implement storae methods
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- generic-storage/src/main.rs
+++ generic-storage/src/main.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Serialize, de::DeserializeOwned};
 use wincode::{SchemaRead, SchemaWrite, config::DefaultConfig};
@@ -10,6 +12,12 @@ trait Serializer<T> {
 struct Borsh;
 struct Wincode;
 struct SerdeJson;
+
+struct Storage<T, S> {
+    data: Option<Vec<u8>>,
+    serializer: S,
+    _marker: PhantomData<T>,
+}
 
 impl<T: BorshSerialize + BorshDeserialize> Serializer<T> for Borsh {
     fn to_bytes(&self, value: &T) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
@@ -41,6 +49,33 @@ impl<T: Serialize + DeserializeOwned> Serializer<T> for SerdeJson {
     }
 }
 
+impl<T, S: Serializer<T>> Storage<T, S> {
+    fn new(serializer: S) -> Self {
+        Storage {
+            data: None,
+            serializer,
+            _marker: PhantomData,
+        }
+    }
+
+    fn has_data(&self) -> bool {
+        self.data.is_some()
+    }
+
+    fn save(&mut self, value: &T) -> Result<(), Box<dyn std::error::Error>> {
+        let bytes = self.serializer.to_bytes(value)?;
+        self.data = Some(bytes);
+        Ok(())
+    }
+
+    fn load(&self) -> Result<T, Box<dyn std::error::Error>> {
+        match &self.data {
+            Some(bytes) => self.serializer.from_bytes(bytes),
+            None => Err("no data stored".into()),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    todo!();
 }
